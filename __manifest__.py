bf3437ff7a712bafc212ea9a3cfd3f0d39b23fa8
Brooks Allan González Lau <brooks@mbaconsultings.com>
2025-12-25 19:03:21 -0500

    Initial module skeleton for Panama EDI Digifact (Odoo 19 Community)

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Panama Electronic Invoicing – Digifact",
+    "version": "19.0.1.0.0",
+    "category": "Accounting/Localization",
+    "summary": "Electronic invoicing for Panama using Digifact (Odoo 19 Community)",
+    "depends": ["account"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
